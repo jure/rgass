0bac64fd98f898369eb189f6421e2530deb0f9e9
Jure Triglav <juretriglav@gmail.com>
2017-04-24 15:57:40 +0200

    Add tests for local insertions and their remote synchronization

--- test/rgass_test.js
+++ test/rgass_test.js
@@ -1,13 +1,110 @@
-const Rgass = require('../rgass.js')
+const View = require('../rgass').View
+const Model = require('../rgass').Model
 
-describe('initial model', () => {
-  let model = new Rgass.Model()
+const op1 = {
+  'type': 'insert',
+  'position': 0,
+  'str': 'a',
+  'key': {
+    'session': 1,
+    'ssv': 1,
+    'site': 1,
+    'offset': 0,
+    'length': 1
+  }
+}
 
-  it('can perform a local insertion', () => {
-    model.LocalInsert(0, 0, 'hello', 'keyCur')
+const op2 = {
+  'type': 'insert',
+  'position': 1,
+  'str': 'b',
+  'key': {
+    'session': 1,
+    'ssv': 2,
+    'site': 1,
+    'offset': 1,
+    'length': 1
+  },
+  'targetKey': {
+    'session': 1,
+    'ssv': 1,
+    'site': 1,
+    'offset': 0,
+    'length': 1
+  }
+}
+
+const op3 = {
+  'type': 'insert',
+  'position': 0,
+  'str': 'c',
+  'key': {
+    'session': 1,
+    'ssv': 3,
+    'site': 1,
+    'offset': 0,
+    'length': 1
+  }
+}
+
+const op4 = {
+  'type': 'insert',
+  'position': 1,
+  'str': 'd',
+  'key': {
+    'session': 1,
+    'ssv': 4,
+    'site': 1,
+    'offset': 1,
+    'length': 1
+  },
+  'targetKey': {
+    'session': 1,
+    'ssv': 3,
+    'site': 1,
+    'offset': 0,
+    'length': 1
+  }
+}
+
+describe('Model', () => {
+  it('can perform local insertion operations', () => {
+    let model = new Model({
+      siteId: 1,
+      session: 1
+    })
+
+    model.applyOperations([op1])
+    model.applyOperations([op2])
+    model.applyOperations([op3])
+    model.applyOperations([op4])
+
+    let view = new View()
+    view.synchronize(model)
+
+    expect(view.toString()).toEqual('cdab')
   })
 
-  it('can perform a remote insertion', () => {
-    model.RemoteInsert(0, 'keyTar missing', 'hello', 'keyCur missing')
+  it('can synchronize with remote sites', () => {
+    let remoteModel = new Model({
+      siteId: 2,
+      session: 1
+    })
+
+    let localModel = new Model({
+      siteId: 1,
+      session: 1,
+      broadcast: (operations) => remoteModel.applyRemoteOperations(operations)
+    })
+
+    localModel.applyOperations([op1])
+    localModel.applyOperations([op2])
+    localModel.applyOperations([op3])
+    localModel.applyOperations([op4])
+
+    let view = new View()
+    view.synchronize(remoteModel)
+
+    expect(view.toString()).toEqual('cdab')
   })
 })
